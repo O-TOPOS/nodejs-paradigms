fc0547cfb4a3a8de762429fbecca2d9ebd35afa6
joe <i.kolovou@spacesyntax.com>
2017-07-29 20:07:37 +0100

    deleted files

--- Basics/0.CreateServer/server.js
+++ Basics/0.CreateServer/server.js
@@ -1,20 +1,10 @@
-var express = require('express')
-  , logger = require('morgan')
-  , app = express()
-  , template = require('jade').compileFile(__dirname + '/source/templates/homepage.jade')
+var http = require('http');
 
-app.use(logger('dev'))
-app.use(express.static(__dirname + '/static'))
+function onRequest(request, response){
+  response.writeHead(200, {'Content-Type':'text/plain'});
+  response.write('Hello World!');
+  response.end();
+}
 
-app.get('/', function (req, res, next) {
-  try {
-    var html = template({ title: 'Home' })
-    res.send(html)
-  } catch (e) {
-    next(e)
-  }
-})
-
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Listening on http://localhost:' + (process.env.PORT || 3000))
-})
+// out-sourced function
+http.createServer(onRequest).listen(8080);
