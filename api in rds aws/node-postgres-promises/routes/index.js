ffad6f0dcaa7f2303b91ef65dbbd7d5649e32184
joe <i.kolovou@spacesyntax.com>
2018-05-12 17:42:36 +0100

    first map

--- api in rds aws/node-postgres-promises/routes/index.js
+++ api in rds aws/node-postgres-promises/routes/index.js
@@ -14,4 +14,21 @@ router.get('/api/puppies', db.getAllPuppies);
 //router.put('/api/puppies/:id', db.updatePuppy);
 //router.delete('/api/puppies/:id', db.removePuppy);
 
+router.get('/map', function(req, res) {
+    //var client = new Client(conString); // Setup our Postgres Client
+    //client.connect(); // connect to the client
+    //var query = db.query(new Query(coffee_query)); // Run our Query
+    //query.on("row", function (row, result) {
+    //    result.addRow(row);
+    //});
+    // Pass the result to the map page
+    //query.on("end", function (result) {
+    //    var data = result.rows[0].row_to_json // Save the JSON as variable data
+        res.render('map', {
+            title: "Express API"//, // Give a title to our page
+            //jsonData: data // Pass data to the View
+        });
+    //});
+});
+
 module.exports = router;
